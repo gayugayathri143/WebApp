9fd995a3c52460dd9ed93d674ab4f0e188f6c8af
gayugayathri143 <68344916+gayugayathri143@users.noreply.github.com>
2020-07-17 09:02:21 +0530

    Update cancelpage.java

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://13.212.8.49:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://18.222.40.156/postgres";
 					String username="postgres";
 					String password="";
 					
